cd91657a80d3effc5071ee35e697952810cb5326
Elias Breitenbach <elias.b@me.com>
2023-06-20 21:23:49 +0200

    Create a project on github

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Elias Breitenbach on 20.06.23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
